e04d01f25ea45ab99b8516595358403b53259cb6
Enzo Rodriguez <lab.er.intro@gmail.com>
2021-02-22 19:02:57 -0300

    My first commit

---
+++ simplycalc.py
@@ -0,0 +1,35 @@
+def arp(val1,val2,oper):
+    if oper == '+':
+        res = val1 + val2
+        return res
+    if oper == '*':
+        res = val1 * val2
+        return res
+    if oper == '-':
+        res == val1 - val2
+        return res
+    if oper == '/':
+        res = val1 / val2
+        return res
+
+
+print("Ingrese el primer valor")
+val1=input()
+va=val1.isnumeric()
+if va:   
+    val1=int(va)
+    print("Ingrese un segundo valor")
+    va2=input()
+    if va2.isnumeric():
+        val2=int(va2)
+        print("Ingrese un operador +, *, -, /")
+        oper = input()
+        #print("La suma de estos números es: " + str(val1+val2))
+        res = arp(val1,val2,oper)
+        #res = maxi(val1)
+        print("La suma de estos números es: " + str(res))
+        input()
+    else:
+        print("El segundo valor ingresado no fue numérico")
+else:
+    print("no ingreso un valor númerico")
